235add4191db3ee90605e6d00f5157b39ca14986
rafaelscdev <128648524+rafaelscdev@users.noreply.github.com>
2024-02-18 19:31:07 -0300

    Add file.

---
+++ script.js
@@ -0,0 +1,12 @@
+let totalRuins = 0;
+let contador = 1;
+while(contador <= 4){
+    let resposta = prompt("Qual a sua avaliação para a serie stranger things? (bom), (rum) ou (excelente)?");
+    if(resposta == "ruim") {
+        totalRuins++;
+    }
+
+    contador++/
+}
+
+console.log(`Total de usuários que avaliou a serie como ruim: ${totalRuins}`);
